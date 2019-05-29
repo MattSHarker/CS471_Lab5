b6ef023b091d5c30b9c87bb484e57faa5f70b6eb
Matt Harker <mattsharker@gmail.com>
2019-05-28 22:16:44 -0700

    Updated main for newest code structure

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,8 @@
 
-#include <iostream>
-
-#include "Permutation.h"
-#include "Matrix.h"
-#include "Memory.h"
 #include "flowshop.h"
 
 int main()
 {
-    runFlowshop();
+    run();
     return 0;
 }
